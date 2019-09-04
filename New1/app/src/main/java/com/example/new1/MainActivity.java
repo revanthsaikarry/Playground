ec5c3674dd3e9625417fed0f19d3df42ab520121
Revanth Sai <krevanth.cs@gmail.com>
2019-09-04 11:14:08 +0530

    My Request page with only relative layout.

--- New1/app/src/main/java/com/example/new1/MainActivity.java
+++ New1/app/src/main/java/com/example/new1/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.new1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button new_req;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new_req = findViewById(R.id.button3);
+        new_req.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RelativeTask.class);
+                startActivity(intent);
+            }
+        });
     }
 }
